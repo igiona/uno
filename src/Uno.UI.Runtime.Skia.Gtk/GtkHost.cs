dd73c23b5aef3491a5e53ac267c30cbd62fcf6f6
Martin Zikmund <martin@zikmund.dev>
2023-01-09 13:08:52 +0100

    refactor: Use XamlRootMap on GTK

--- src/Uno.UI.Runtime.Skia.Gtk/GtkHost.cs
+++ src/Uno.UI.Runtime.Skia.Gtk/GtkHost.cs
@@ -33,6 +33,7 @@ using Uno.Disposables;
 using System.Collections.Generic;
 using Uno.Extensions.ApplicationModel.Core;
 using Windows.ApplicationModel;
+using Uno.UI.XamlHost.Skia.Gtk.Hosting;
 
 namespace Uno.UI.Runtime.Skia
 {
@@ -55,15 +56,6 @@ namespace Uno.UI.Runtime.Skia
 		private record PendingWindowStateChangedInfo(Gdk.WindowState newState, Gdk.WindowState changedMask);
 		private List<PendingWindowStateChangedInfo> _pendingWindowStateChanged = new();
 
-		public static Gtk.Window Window => _window;
-		internal static UnoEventBox EventBox => _eventBox;
-
-		/// <summary>
-		/// Gets or sets the current Skia Render surface type.
-		/// </summary>
-		/// <remarks>If <c>null</c>, the host will try to determine the most compatible mode.</remarks>
-		public RenderSurfaceType? RenderSurfaceType { get; set; }
-
 		/// <summary>
 		/// Creates a host for a Uno Skia GTK application.
 		/// </summary>
@@ -82,6 +74,18 @@ namespace Uno.UI.Runtime.Skia
 		{
 			_appBuilder = appBuilder;
 		}
+
+		public static Gtk.Window Window => _window;
+
+		internal static UnoEventBox EventBox => _eventBox;
+
+		internal IRenderSurface RenderSurface => _renderSurface;
+
+		/// <summary>
+		/// Gets or sets the current Skia Render surface type.
+		/// </summary>
+		/// <remarks>If <c>null</c>, the host will try to determine the most compatible mode.</remarks>
+		public RenderSurfaceType? RenderSurfaceType { get; set; }
 
 		public void Run()
 		{
@@ -349,6 +353,7 @@ namespace Uno.UI.Runtime.Skia
 				throw new InvalidOperationException("XamlRoot was not properly initialized");
 			}
 
+			XamlRootMap.Register(xamlRoot, this);
 			xamlRoot.InvalidateRender += _renderSurface.InvalidateRender;
 
 			CoreServices.Instance.ContentRootCoordinator.CoreWindowContentRootSet -= OnCoreWindowContentRootSet;
